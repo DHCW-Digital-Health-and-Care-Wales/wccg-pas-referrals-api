223856f90b15508089b9c124089f37fad1869d10
Patryk Urban <kai_patrykurban@wales.nhs.uk>
2025-02-06 11:24:08 +0100

    Feature/project template (#1)
    * Added sample project

---
+++ test/WCCG.PAS.ReferralsAPI.Unit.Tests/Example/UnitTest1.cs
@@ -0,0 +1,12 @@
+using FluentAssertions;
+
+namespace WCCG.PAS.ReferralsAPI.Unit.Tests.Example;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        true.Should().BeTrue();
+    }
+}
